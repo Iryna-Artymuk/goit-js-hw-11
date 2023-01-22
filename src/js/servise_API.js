626744fcd8aa4711eab2e4ab6bc0c31e63c4b6e7
Iryna Artymuk <artimyk@ukr.net>
2023-01-22 21:46:08 +0200

    Update servise_API.js

--- src/js/servise_API.js
+++ src/js/servise_API.js
@@ -7,10 +7,10 @@ export default class FetchAPI {
     this.pictureArr = [];
   }
 
-  fetchPictures() {
+  async fetchPictures() {
     const KEY = '32771968-7fd567c901afb84ab6320145c';
     const URL = 'https://pixabay.com/api/';
-    return axios.get(
+    return await axios.get(
       `https://pixabay.com/api/?key=${KEY}&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${this.page}`
     );
   }
