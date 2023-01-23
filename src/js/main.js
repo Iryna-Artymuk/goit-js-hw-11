11851c926363f43885244eb82b05c257ecd04d5d
Iryna Artymuk <artimyk@ukr.net>
2023-01-23 16:00:11 +0200

    updated api servise
    add options

--- src/js/main.js
+++ src/js/main.js
@@ -9,33 +9,18 @@ const fetchAPI = new FetchAPI();
 const form = document.querySelector('#search-form');
 const gallery = document.querySelector('.gallery');
 const guard = document.querySelector('.guard');
+
 form.addEventListener('submit', getPictureOnSubmit);
-const pixabay_URL = 'https://pixabay.com/api/';
-const KEY = '32771968-7fd567c901afb84ab6320145c';
-const pixabay_options = {
-  params: {
-    key: KEY,
-    q: fetchAPI.searchQuery,
-    image_type: 'photo',
-    orientation: 'horizontal',
-    safesearch: true,
-    per_page: 40,
-    page: fetchAPI.page,
-  },
-};
 
-fetchAPI.BASE_URL = pixabay_URL;
-
-fetchAPI.options = pixabay_options;
 const observer_options = {
   root: null,
   rootMargin: '300px',
   threshold: 1.0,
 };
 const observer = new IntersectionObserver(loadMoreOnScroll, observer_options);
+
 function getPictureOnSubmit(event) {
   event.preventDefault();
-
   fetchAPI.searchQuery = event.currentTarget.elements.searchQuery.value.trim();
   fetchAPI.resetPage();
   gallery.innerHTML = '';
@@ -75,7 +60,6 @@ function loadMoreOnScroll(entries, observer) {
   entries.forEach(entry => {
     if (entry.isIntersecting) {
       fetchAPI.updatePage();
-
       fetchAPI
         .fetchPictures()
         .then(resp => {
--- src/js/servise_API.js
+++ src/js/servise_API.js
@@ -5,14 +5,22 @@ export default class FetchAPI {
     this.searchQuery = '';
     this.page = 1;
     this.pictureArr = [];
+    this.URL = 'https://pixabay.com/api/';
   }
 
   async fetchPictures() {
-    const KEY = '32771968-7fd567c901afb84ab6320145c';
-    const URL = 'https://pixabay.com/api/';
-    return await axios.get(
-      `https://pixabay.com/api/?key=${KEY}&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${this.page}`
-    );
+    const options = {
+      params: {
+        key: '32771968-7fd567c901afb84ab6320145c',
+        q: this.searchQuery,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: 'safesearch',
+        page: this.page,
+        per_page: 40,
+      },
+    };
+    return await axios.get(this.URL, options);
   }
 
   updatePage() {
